c4f8b6b1ce3044d23cbcad9d5527c5114acb55a6
RSNirwan <rajbir.nirwan@gmail.com>
2021-10-29 15:36:34 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sh",
+    version="1.0.0",
+    url="",
+    author="Author Name",
+    author_email="author@gmail.com",
+    description="Description of my package",
+    packages=find_packages(),
+    install_requires=["pytest"],
+)
